08c8e2b21610af7b556271677ea0fae9e4d9a7f6
Lucas Ferreira <lucas.santos61@fatec.sp.gov.br>
2018-01-06 21:07:27 -0200

    Add release page not found

--- core/Container.php
+++ core/Container.php
@@ -7,4 +7,14 @@ class Container
         $controller = "\\App\\Controllers\\".$controller;
         return new $controller;
     }
+
+    public static function pageNotFound()
+    {
+        if(file_exists(__DIR__."/../app/Views/404.phtml")){
+            return require_once __DIR__."/../app/Views/404.phtml";
+        }
+
+        echo "page not exists";
+        return;
+    }
 }
