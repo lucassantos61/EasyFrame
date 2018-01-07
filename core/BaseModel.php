6b5a43a7923dbff7fc4f56e33dd7ba23b645eaa8
Lucas Ferreira <lucas.santos61@fatec.sp.gov.br>
2018-01-07 18:17:57 -0200

    Start models

---
+++ core/BaseModel.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Core;
+
+use PDO;
+
+abstract class BaseModel
+{
+    private $pdo;
+    protected $table;
+
+    public function __construct(PDO $pdo){
+        $this->pdo = $pdo;
+    }
+}   
