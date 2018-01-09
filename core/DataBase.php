454978dedb8c30fefab3aca8f179d2ec86eda9d3
Lucas Ferreira <lucas.santos61@fatec.sp.gov.br>
2018-01-08 22:12:19 -0200

    Bugfix

--- core/DataBase.php
+++ core/DataBase.php
@@ -6,7 +6,7 @@ use PDO;
 use PDOException;
 class DataBase
 {
-    public function getDataBase()
+    public static function getDataBase()
     {
         $conf = include_once(__DIR__."/../app/database.php");
 
